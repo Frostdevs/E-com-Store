2e549168f5415c9832308815a70499153e028ad6
Frostdevs <adesanyanifemi1@gmail.com>
2025-11-06 17:48:39 -0700

    crumbs modified

--- src/Components/Crumbs/Crumbs.jsx
+++ src/Components/Crumbs/Crumbs.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Crumbs = ({text, secondText, color}) => {
   return (
     <ul className=" w-full justify-center flex gap-2 bg-[#F6F7F8] font-[Proxima Nova] font-lg font-normal" style={{paddingBlock: 8}}>
-      <NavLink className=" text-[#33A0FF]" to="/">Home</NavLink>/<li style={{color}}>{text}</li><li >{secondText}</li>
+      <NavLink className=" text-[#33A0FF]" to="/">Home</NavLink>/<NavLink style={{color}} to="/hot">{text}</NavLink><li >{secondText}</li>
     </ul>
   );
 };
